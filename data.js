0e92be251fd6f8ead1959f94d01a7df6d69ddc54
passin0 <149547482+passin0@users.noreply.github.com>
2025-04-15 20:56:33 +0800

    Update data.js 巡洋艦

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-//第一分類:公司  A:未央公司與其他 B:木星工業 C:諾瑪運輸 D:安東尼奧斯
+//第一分類:公司  A:未央公司 B:木星工業 C:諾瑪運輸 D:安東尼奧斯
 //第二分類:戰略性能 A:火力出眾 B:持續作戰 C:戰略與支援 D:戰機與護航艇
 //第三分類:戰術性能 A:投射武器 B:直射武器
 //機率 
@@ -11,37 +11,310 @@ window.itemsData = [
       "category1": ["A"],
       "category2": ["C"],
       "image": "images/object1.png",
-      "weight": 1
+      "weight": 10
     },
     {
       "name": "太陽鯨",
       "category1": ["C"],
       "category2": ["C"],
       "image": "",
-      "weight": 1
+      "weight": 10
     },
     {
       "name": "永恆蒼芎",
       "category1": ["B"],
       "category2": ["A","C"],
       "image": "images/object2.png",
-      "weight": 1
+      "weight": 10
     },
     {
-      "name": "南十字",
+      "name": "南十字星元帥",
       "category1": ["D"],
       "category2": ["A","C"],
-      "category3": ["A"],
+      "category3": ["B"],
       "image": "",
-      "weight": 1
+      "weight": 10
+    },
+    {//支援艦
+      "name": "埃迪卡拉",
+      "category1": ["C"],
+      "category2": ["A","C"],
+      "image": "",
+      "weight": 5
+    },
+    {
+      "name": "FSV830",
+      "category1": ["A"],
+      "category2": ["B","C"],
+      "image": "",
+      "weight": 5
     },
     {//戰巡
-      "name": "物件5",
+      "name": "新君士坦丁大帝",
+      "category1": ["D"],
+      "category2": ["A"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 21
+    },
+    {
+      "name": "永恆風暴",
+      "category1": ["B"],
+      "category2": ["A"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 21
+    },
+    {
+      "name": "普魯圖斯之盾",
+      "category1": ["C"],
+      "category2": ["A","B"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 21
+    },
+    {
+      "name": "烏拉諾斯之矛",
+      "category1": ["B"],
+      "category2": ["A","C"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 21
+    },
+    {
+      "name": "ST59",
       "category1": ["A"],
+      "category2": ["B","C"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 21
+    },
+    {
+      "name": "雷火之星",
+      "category2": ["A","C"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 21
+    },
+    {//巡洋艦
+      "name": "卡利斯托A",
+      "category1": ["B"],
       "category2": ["A"],
       "category3": ["A"],
       "image": "",
-      "weight": 25
+      "weight": 236
+    },
+    {
+      "name": "卡利斯托B",
+      "category1": ["B"],
+      "category3": ["A"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "卡利斯托C",
+      "category1": ["B"],
+      "category3": ["A"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "遊騎兵A",
+      "category1": ["D"],
+      "category2": ["A"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "遊騎兵B",
+      "category1": ["D"],
+      "category2": ["A"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "狩獵者A",
+      "category1": ["D"],
+      "category2": ["C"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "狩獵者B",
+      "category1": ["D"],
+      "category2": ["C"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "狩獵者C",
+      "category1": ["D"],
+      "category2": ["C"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "光錐A",
+      "category1": ["C"],
+      "category3": ["A"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "光錐B",
+      "category1": ["C"],
+      "category2": ["C"],
+      "category3": ["A"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "光錐C",
+      "category1": ["C"],
+      "category2": ["A"],
+      "category3": ["A"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "KCCPV2.0A",
+      "category1": ["A"],
+      "category2": ["A", "C"],
+      "category3": ["A"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "KCCPV2.0B",
+      "category1": ["A"],
+      "category2": ["A"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "KCCPV2.0C",
+      "category1": ["A"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "KCCPV2.0D",
+      "category1": ["A"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "獵兵A",
+      "category1": ["D"],
+      "category2": ["C"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "獵兵B",
+      "category1": ["D"],
+      "category2": ["C"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "伊俄A",
+      "category1": ["B"],
+      "category2": ["A", "B"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "伊俄B",
+      "category1": ["B"],
+      "category2": ["A", "B"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "伊俄C",
+      "category1": ["B"],
+      "category2": ["A", "B"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "康納馬拉混沌A",
+      "category1": ["B"],
+      "category2": ["C"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "康納馬拉餛飩B",
+      "category1": ["B"],
+      "category2": ["C"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "奇美拉A",
+      "category1": ["C"],
+      "category2": ["A", "B"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "奇美拉B",
+      "category1": ["C"],
+      "category2": ["A", "B"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "奇美拉C",
+      "category1": ["C"],
+      "category2": ["B"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "CAS066A",
+      "category1": ["A"],
+      "category2": ["A", "B"],
+      "category3": ["A"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "CAS066B",
+      "category1": ["A"],
+      "category2": ["A"],
+      "category3": ["B"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "CAS066C",
+      "category1": ["A"],
+      "image": "",
+      "weight": 236
+    },
+    {
+      "name": "CAS066D",
+      "category1": ["A"],
+      "category2": ["C"],
+      "image": "",
+      "weight": 236
     }
+    
   ];
   
